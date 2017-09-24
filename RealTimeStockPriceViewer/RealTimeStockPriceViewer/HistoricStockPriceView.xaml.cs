64d2d572bce65a8d5fee331d8b368662e0dcd34d
beroshan26 <beroshan26@users.noreply.github.com>
2017-09-24 18:03:28 +0530

    Removed mainwindow added stock view and historic views

---
+++ RealTimeStockPriceViewer/RealTimeStockPriceViewer/HistoricStockPriceView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RealTimeStockPriceViewer
+{
+    /// <summary>
+    /// Interaction logic for HistoricStockPriceView.xaml
+    /// </summary>
+    public partial class HistoricStockPriceView : Window
+    {
+        public HistoricStockPriceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
